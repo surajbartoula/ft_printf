8d16757c30edb652e8d8f3cf79abc90bfc08dbd6
Suraj Bartoula <suraj_bartoula@yahoo.com>
2024-01-02 16:18:17 +0400

    done
    final changes

---
+++ include/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 21:42:09 by sbartoul          #+#    #+#             */
+/*   Updated: 2024/01/01 21:48:50 by sbartoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int				ft_printf(const char *format, ...);
+int				ft_format(char specifier, va_list ap);
+
+#endif
